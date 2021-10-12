74d87a15947be9ff8d07806a20c48ceab705df69
Markus Schmidt <m.schmidt@iwm-tuebingen.de>
2021-10-12 17:58:34 +0200

    Changed: Install introductions

--- controllers/default.py
+++ controllers/default.py
@@ -8,11 +8,15 @@
 #
 # INSTALL
 #
-# cd /usr/lib/
 # apt install ffmpeg
+# apt install python3-pip
+# pip3 install --upgrade pip
+# cd /usr/lib/
 # git clone --recursive https://github.com/web2py/web2py.git
-# cd web2py
-# cd web2py/applications/transcription
+# cd web2py/applications
+# mkdir transcription
+# cd transcription
+# git clone https://gitea.iwm-tuebingen.de/mschmidt/web2py.transcription.git .
 # pip3 install -t modules srt
 # pip3 install -t modules vosk
 # pip3 install -t modules webvtt-py
